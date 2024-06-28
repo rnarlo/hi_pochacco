50571656ed9ee2f86a0f85e23fe98e842d68867e
rnarlo <cgjimenez4@up.edu.ph>
2024-06-28 15:48:58 +0800

    test

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -102,6 +102,66 @@ function parseOBJ(text) {
   };
 }
 
+function parseMTL(text) {
+  const materials = {};
+  let material;
+
+  const keywords = {
+    newmtl(parts, unparsedArgs) {
+      material = {};
+      materials[unparsedArgs] = material;
+    },
+    /* eslint brace-style:0 */
+    Ns(parts) {
+      material.shininess = parseFloat(parts[0]);
+    },
+    Ka(parts) {
+      material.ambient = parts.map(parseFloat);
+    },
+    Kd(parts) {
+      material.diffuse = parts.map(parseFloat);
+    },
+    Ks(parts) {
+      material.specular = parts.map(parseFloat);
+    },
+    Ke(parts) {
+      material.emissive = parts.map(parseFloat);
+    },
+    Ni(parts) {
+      material.opticalDensity = parseFloat(parts[0]);
+    },
+    d(parts) {
+      material.opacity = parseFloat(parts[0]);
+    },
+    illum(parts) {
+      material.illum = parseInt(parts[0]);
+    },
+  };
+
+  const keywordRE = /(\w*)(?: )*(.*)/;
+  const lines = text.split("\n");
+  for (let lineNo = 0; lineNo < lines.length; ++lineNo) {
+    const line = lines[lineNo].trim();
+    if (line === "" || line.startsWith("#")) {
+      continue;
+    }
+    const m = keywordRE.exec(line);
+    if (!m) {
+      continue;
+    }
+    const [, keyword, unparsedArgs] = m;
+    const parts = line.split(/\s+/).slice(1);
+    const handler = keywords[keyword];
+    if (!handler) {
+      console.warn("unhandled keyword:", keyword); // eslint-disable-line no-console
+      continue;
+    }
+    handler(parts, unparsedArgs);
+  }
+
+  return materials;
+}
+
 function getRandomFloat(min, max, decimals) {
   const str = (Math.random() * (max - min) + min).toFixed(decimals);
   return parseFloat(str);
@@ -146,20 +206,27 @@ async function main() {
    */
 
   const vs = `
-  attribute vec4 a_position;
-  attribute vec3 a_normal;
+    attribute vec4 a_position;
+    attribute vec3 a_normal;
+    attribute vec4 a_color;
 
-  uniform mat4 u_matrix;
-  uniform mat4 u_transformation;
-  uniform mat4 u_world;
+    uniform mat4 u_projection;
+    uniform mat4 u_view;
+    uniform mat4 u_world;
+    uniform vec3 u_viewWorldPosition;
 
-  varying vec3 v_normal;
+    varying vec3 v_normal;
+    varying vec3 v_surfaceToView;
+    varying vec4 v_color;
 
-  void main() {
-    gl_Position = u_transformation * u_matrix * a_position;
-    v_normal = mat3(u_world) * a_normal;
-  }
-  `;
+    void main() {
+      vec4 worldPosition = u_world * a_position;
+      gl_Position = u_projection * u_view * worldPosition;
+      v_surfaceToView = u_viewWorldPosition - worldPosition.xyz;
+      v_normal = mat3(u_world) * a_normal;
+      v_color = a_color;
+    }
+    `;
 
   const smoothVs = `
   attribute vec4 a_position;
@@ -178,37 +245,55 @@ async function main() {
   `;
 
   const fs = `
-  precision mediump float;
+    precision highp float;
 
-  varying vec3 v_normal;
+    varying vec3 v_normal;
+    varying vec3 v_surfaceToView;
+    varying vec4 v_color;
 
-  uniform vec4 u_diffuse;
-  uniform vec3 u_lightDirection;
+    uniform vec3 diffuse;
+    uniform vec3 ambient;
+    uniform vec3 emissive;
+    uniform vec3 specular;
+    uniform float shininess;
+    uniform float opacity;
+    uniform vec3 u_lightDirection;
+    uniform vec3 u_ambientLight;
 
-  void main () {
-    vec3 normal = normalize(v_normal);
-    float fakeLight = dot(u_lightDirection, normal) * .5 + .5;
-    gl_FragColor = vec4(u_diffuse.rgb * fakeLight, u_diffuse.a);
-  }
-  `;
+    void main () {
+      vec3 normal = normalize(v_normal);
+
+      vec3 surfaceToViewDirection = normalize(v_surfaceToView);
+      vec3 halfVector = normalize(u_lightDirection + surfaceToViewDirection);
+
+      float fakeLight = dot(u_lightDirection, normal) * .5 + .5;
+      float specularLight = clamp(dot(normal, halfVector), 0.0, 1.0);
+
+      vec3 effectiveDiffuse = diffuse * v_color.rgb;
+      float effectiveOpacity = opacity * v_color.a;
+
+      gl_FragColor = vec4(
+          emissive +
+          ambient * u_ambientLight +
+          effectiveDiffuse * fakeLight +
+          specular * pow(specularLight, shininess),
+          effectiveOpacity);
+    }
+    `;
 
   // compiles and links the shaders, looks up attribute and uniform locations
   // Regular cube-like normals
   // const meshProgramInfo = webglUtils.createProgramInfo(gl, [vs, fs]);
   // Smooth lighting normals
-  const meshProgramInfo = webglUtils.createProgramInfo(gl, [smoothVs, fs]);
+  const meshProgramInfo = webglUtils.createProgramInfo(gl, [vs, fs]);
   console.log("is error?");
 
   // Pochacco
-  const pochacco = await fetch("../object_files/pochacco.obj");
+  const pochacco = await fetch(
+    "https://raw.githubusercontent.com/rnarlo/hi_pochacco/master/src/object_files/pochacco.obj",
+  );
   const pochaccoText = await pochacco.text();
   const pochaccoData = parseOBJ(pochaccoText);
-
-  const planetResponse = await fetch(
-    "https://raw.githubusercontent.com/jfeching/161_interactive_screensaver/main/src/object_files/yellow_ball.obj",
-  );
-  const planetText = await planetResponse.text();
-  const planetData = parseOBJ(planetText);
 
   // Because moon1Data is just named arrays like this
   //
@@ -228,10 +313,10 @@ async function main() {
     gl,
     pochaccoData,
   );
-  const planetBufferInfo = webglUtils.createBufferInfoFromArrays(
-    gl,
-    planetData,
-  );
+  // const planetBufferInfo = webglUtils.createBufferInfoFromArrays(
+  //   gl,
+  //   planetData,
+  // );
 
   let transformationMatrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
 
@@ -441,33 +526,33 @@ async function main() {
      * PLANET
      * Procedure is same except no translate and revolve
      */
-    var planetTranslate = [0, 0, 0];
-    var planetRotate = -time;
-    var planetRevolve = 0;
+    // var planetTranslate = [0, 0, 0];
+    // var planetRotate = -time;
+    // var planetRevolve = 0;
 
-    const planetUniforms = {
-      u_lightDirection: m4.normalize([-1, 3, 5]),
-      u_matrix: computeMatrix(
-        viewProjectionMatrix,
-        planetTranslate,
-        planetRotate,
-        planetRevolve,
-      ),
-    };
+    // const planetUniforms = {
+    //   u_lightDirection: m4.normalize([-1, 3, 5]),
+    //   u_matrix: computeMatrix(
+    //     viewProjectionMatrix,
+    //     planetTranslate,
+    //     planetRotate,
+    //     planetRevolve,
+    //   ),
+    // };
 
-    webglUtils.setUniforms(meshProgramInfo, planetUniforms);
-    webglUtils.setBuffersAndAttributes(gl, meshProgramInfo, planetBufferInfo);
-    webglUtils.setUniforms(meshProgramInfo, {
-      u_world: m4.multiply(
-        m4.yRotation(planetRotate),
-        m4.yRotation(planetRevolve),
-      ),
-      u_diffuse: colors[2],
-      u_lightDirection: [ldx, ldy, ldz],
-      u_transformation: transformationMatrix,
-    });
+    // webglUtils.setUniforms(meshProgramInfo, planetUniforms);
+    // webglUtils.setBuffersAndAttributes(gl, meshProgramInfo, planetBufferInfo);
+    // webglUtils.setUniforms(meshProgramInfo, {
+    //   u_world: m4.multiply(
+    //     m4.yRotation(planetRotate),
+    //     m4.yRotation(planetRevolve),
+    //   ),
+    //   u_diffuse: colors[2],
+    //   u_lightDirection: [ldx, ldy, ldz],
+    //   u_transformation: transformationMatrix,
+    // });
 
-    webglUtils.drawBufferInfo(gl, planetBufferInfo);
+    // webglUtils.drawBufferInfo(gl, planetBufferInfo);
 
     /**
      * Pochacco
